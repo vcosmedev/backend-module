f74fd547a90a4858bbde4fc0112782ea87c120c2
Víctor Cosme <victorcosmema@gmail.com>
2022-09-28 19:13:28 -0500

    Add class number 7

--- 04-promises/practice-promises.js
+++ 04-promises/practice-promises.js
@@ -1,19 +1,8 @@
 /*
-Prácticas: 
+Práctica: 
     1. Realizar el proceso de inscripción a Kodemia con promesas
         - Encadenamiento de promesas
         - async / await
-    2. Práctica del CRUD de Koders con file System con promesas:
-        A partir de archivo koders.json
-        Realizar lo siguiente:
-            1º Crear un función que permita leer el archivo e imprimir en consola el arreglo de Koders
-            2º Crear una función que permita agregar un Koder y guardar el archivo con el cambio realizado
-            3º Crear una función que permita eliminar a un Koder por su id y guardar el archivo con el cambio realizado
-            4º Crear una función que permita editar a un Koder por su id y guardar el archivo con el cambio realizado
-            5º Crear una función que permita obtener a los koders que sean mayores a 25 años
-            JSON.parse() -> convierte de cadena a un objeto js
-            JSON.stringify() -> convertir de objeto a string
-            async / await
 */
 
 
@@ -138,6 +127,4 @@ main() // Cuando una función es asíncrona, devuelve una promesa por lo que se 
     .catch((error) => {
         console.log('Error: ', error)
     });
-
-
 
